128694d4106710622d8297fa55e5d2b820fe27dc
omwi <bsuiromwi@gmail.com>
2022-10-07 22:02:49 +0300

    registered models for admin

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Word, Meaning
+
+admin.site.register(User)
+admin.site.register(Word)
+admin.site.register(Meaning)

a4758caf4d7a979b59fd3d0160d354734c382360
omwi <bsuiromwi@gmail.com>
2022-10-12 17:05:19 +0300

    changed admin model for user

--- bot/admin.py
+++ bot/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import User, Word, Meaning
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'name', 'role',)
+    readonly_fields = ('chat_id',)
+    search_fields = ('name',)
+    fieldsets = (
+        (None, {
+            'fields': ('chat_id', 'name', 'role')
+        }),
+    )
 admin.site.register(Word)
+
+
 admin.site.register(Meaning)
